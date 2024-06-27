776607e1a7d7e895889f5bd1e4629dcb9187d3c2
hafiziN88 <hafizi.nordin@ikano.asia>
2024-06-27 15:24:34 +0800

    Add article logic

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('articles', [ArticleController::class, 'index']);
+/*
+Route::get('articles/{id}', 'ArticleController@show');
+Route::post('articles', 'ArticleController@store');
+Route::put('articles/{id}', 'ArticleController@update');
+Route::delete('articles/{id}', 'ArticleController@delete');
+*/
